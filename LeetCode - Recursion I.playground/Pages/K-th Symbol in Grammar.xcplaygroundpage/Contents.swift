b2e7483064e7f6c52ed454cd4c4ddd960a10bae9
LeiHao <leihao.sg@gmail.com>
2020-02-26 18:53:56 +0800

    Update K-th Symbol in Grammar

--- LeetCode - Recursion I.playground/Pages/K-th Symbol in Grammar.xcplaygroundpage/Contents.swift
+++ LeetCode - Recursion I.playground/Pages/K-th Symbol in Grammar.xcplaygroundpage/Contents.swift
@@ -1,10 +1,25 @@
 //: [Previous](@previous)
 
+//70.59%
+//Runtime: 4 ms
+//Memory Usage: 20.6 MB
+
+
 class Solution {
     func kthGrammar(_ N: Int, _ K: Int) -> Int {
-        
-        return 0
+        if N < 2 { return 0 }
+        return (kthGrammar(N-1, (K+1)/2) == 0) ? 1-K%2 : K%2
     }
 }
+
+let s = Solution()
+s.kthGrammar(1, 1)
+s.kthGrammar(2, 1)
+s.kthGrammar(2, 2)
+s.kthGrammar(4, 5)
+s.kthGrammar(15, 32)
+s.kthGrammar(30, 323241)
+
+
 
 //: [Next](@next)
