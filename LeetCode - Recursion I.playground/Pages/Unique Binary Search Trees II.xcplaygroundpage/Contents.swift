e8a5a2048c791886d4b1ebfe76855ad32c1dadc3
LeiHao <leihao.sg@gmail.com>
2020-02-27 14:52:14 +0800

    Add Unique Binary Search Trees II

--- LeetCode - Recursion I.playground/Pages/Unique Binary Search Trees II.xcplaygroundpage/Contents.swift
+++ LeetCode - Recursion I.playground/Pages/Unique Binary Search Trees II.xcplaygroundpage/Contents.swift
@@ -1,10 +1,42 @@
 //: [Previous](@previous)
 
+//61.54%
+//Runtime: 36 ms
+//Memory Usage: 22.2 MB
+
 class Solution {
     func generateTrees(_ n: Int) -> [TreeNode?] {
+        if n < 1 { return [] }
+        return helper(1, n)
+    }
+    
+    private func helper(_ i1: Int, _ i2:Int) -> [TreeNode?] {
+        if i1 > i2 { return [nil] }
+
+        var tn = [TreeNode]()
+        for i in i1...i2 {
+            let l = helper(i1, i-1)
+            let r = helper(i+1, i2)
+            
+            for li in l {
+                for ri in r {
+                    let n = TreeNode(i)
+                    n.left = li
+                    n.right = ri
+                    tn.append(n)
+                }
+            }
+        }
         
-        return nil
+        return tn
     }
 }
+
+let s = Solution()
+
+for i in -3..<3 {
+    print(i, s.generateTrees(i))
+}
+
 
 //: [Next](@next)
