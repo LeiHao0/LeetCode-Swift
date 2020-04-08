76b4040b4da4f7a941afe83e35e28e1e44f6896b
LeiHao <leihao.sg@gmail.com>
2020-04-08 23:47:54 +0800

    Group Anagrams

---
+++ 30-Day LeetCoding Challenge.playground/Pages/Group Anagrams.xcplaygroundpage/Contents.swift
@@ -0,0 +1,32 @@
+//: [Previous](@previous)
+
+class Solution {
+    
+    //    Runtime: 192 ms, faster than 90.31%
+    //    Memory Usage: 24 MB, less than 33.33%
+    func groupAnagrams(_ strs: [String]) -> [[String]] {
+        var dict = [String: [String]]()
+        strs.forEach {
+            let s = String($0.sorted())
+            dict[s] = (dict[s] == nil) ? [$0] : dict[s]! + [$0]
+        }
+        return dict.map({$0.value})
+    }
+    
+    //    Runtime: 196 ms
+    //    Memory Usage: 24.1 MB
+    //    82.39%
+    func groupAnagrams1(_ strs: [String]) -> [[String]] {
+        var dict = [String:[String]]()
+        zip(strs.map({ String($0.sorted()) }), strs).forEach { k, v in
+            dict[k] == nil ? dict[k] = [v] : dict[k]!.append(v)
+        }
+        return Array(dict.values)
+    }
+}
+
+
+let s = Solution()
+s.groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])
+
+//: [Next](@next)
