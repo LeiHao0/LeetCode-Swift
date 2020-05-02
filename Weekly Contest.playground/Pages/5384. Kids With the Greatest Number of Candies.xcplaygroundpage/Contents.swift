a47477b8680e26ad00aea6f0e64559c3402fadc2
LeiHao <leihao.sg@gmail.com>
2020-05-02 22:42:14 +0800

    Update

---
+++ Weekly Contest.playground/Pages/5384. Kids With the Greatest Number of Candies.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+//: [Previous](@previous)
+
+class Solution {
+    func kidsWithCandies(_ candies: [Int], _ extraCandies: Int) -> [Bool] {
+        if candies.isEmpty { return [] }
+        let m = candies.max()!
+        return candies.map({ ($0 + extraCandies) >= m })
+    }
+}
+
+let s = Solution()
+
+s.kidsWithCandies([], 3)
+s.kidsWithCandies([2,3,5,1,3], 3)
+s.kidsWithCandies([4,2,1,1,2], 1)
+s.kidsWithCandies([12,1,12], 10)
+
+
+//: [Next](@next)
