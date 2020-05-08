db4fbd537593da533ff813c11744f60fdcceae73
LeiHao <leihao.sg@gmail.com>
2020-05-08 23:19:51 +0800

    Update Subarray Sum Equals K

--- LeetCoding Challenge 4.playground/Pages/Subarray Sum Equals K.xcplaygroundpage/Contents.swift
+++ LeetCoding Challenge 4.playground/Pages/Subarray Sum Equals K.xcplaygroundpage/Contents.swift
@@ -39,10 +39,48 @@
  */
 
 class Solution {
+    // 144ms, 33.25%
     func subarraySum(_ nums: [Int], _ k: Int) -> Int {
-        
-        return 0
+        var v = 0, sum = 0, dict = [0: 1]
+        for n in nums {
+            sum += n
+            v += dict[sum-k] ?? 0
+            dict[sum] = (dict[sum] ?? 0) + 1
+        }
+        return v
+    }
+    
+    // 2580 ms, 😂
+    func subarraySum1(_ nums: [Int], _ k: Int) -> Int {
+        var n = 0
+        for i in 0..<nums.count {
+            var sum = 0
+            for j in i..<nums.count {
+                sum += nums[j]
+                if sum == k { n += 1 }
+            }
+        }
+        return n
+    }
+    
+    // 3460ms, 😂😂
+    func subarraySum0(_ nums: [Int], _ k: Int) -> Int {
+        var sums = Array(repeating: 0, count: nums.count+1), n = 0
+        for (i, v) in nums.enumerated() {
+            sums[i+1] = sums[i] + v
+        }
+        for i in 0..<sums.count {
+            for j in i+1..<sums.count {
+                if sums[j] - sums[i] == k { n += 1 }
+            }
+        }
+        return n
     }
 }
+
+let s = Solution()
+s.subarraySum([1,1,1], 2)
+s.subarraySum([1], 0)
+
 
 //: [Next](@next)
