f50e138cc4773be2dcaf70e3e8bb867e43fd424f
LeiHao <leihao.sg@gmail.com>
2020-05-12 17:30:47 +0800

    Finished Arrays 101

--- Arrays 101.playground/Pages/Find All Numbers Disappeared in an Array.xcplaygroundpage/Contents.swift
+++ Arrays 101.playground/Pages/Find All Numbers Disappeared in an Array.xcplaygroundpage/Contents.swift
@@ -1,14 +1,21 @@
 //: [Previous](@previous)
 
 class Solution {
+    // 476ms, 66.22%
     func findDisappearedNumbers(_ nums: [Int]) -> [Int] {
-        
-        return []
+        let sets = Set(nums)
+        var a = [Int]()
+        for i in 0..<nums.count  {
+            if !sets.contains(i+1) { a.append(i+1) }
+        }
+        return a
     }
 }
 
 let s = Solution()
 s.findDisappearedNumbers([4,3,2,7,8,2,3,1])
+s.findDisappearedNumbers([])
+s.findDisappearedNumbers([1,1])
 
 
 //: [Next](@next)
--- Arrays 101.playground/Pages/Max Consecutive Ones II.xcplaygroundpage/Contents.swift
+++ Arrays 101.playground/Pages/Max Consecutive Ones II.xcplaygroundpage/Contents.swift
@@ -1,8 +1,27 @@
 //: [Previous](@previous)
 
 class Solution {
+    // 300ms, 31%
     func findMaxConsecutiveOnes(_ nums: [Int]) -> Int {
-        
+        var z0 = 0, z1 = 0, maxZ = 0
+        for i in nums {
+            if i == 1 {
+                z1 += 1
+            } else {
+                z0 = z1
+                z1 = 0
+            }
+            maxZ = max(maxZ, z0+z1+1)
+        }
+        return min(maxZ, nums.count)
     }
 }
+
+let s = Solution()
+s.findMaxConsecutiveOnes([1,0,1,1,0])
+s.findMaxConsecutiveOnes([1,0,0,1,1,0])
+s.findMaxConsecutiveOnes([0,0,0,0,0,0,1,0])
+s.findMaxConsecutiveOnes([0,0,0,0,0,0,0])
+s.findMaxConsecutiveOnes([1,1,1,1,1])
+
 //: [Next](@next)
