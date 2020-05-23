9d6dae660760de3a25b7d99db7fe704865597c80
LeiHao <leihao.sg@gmail.com>
2020-05-23 18:10:53 +0800

    Add Interval List Intersections

---
+++ LeetCoding Challenge 5.playground/Pages/Interval List Intersections.xcplaygroundpage/Contents.swift
@@ -0,0 +1,53 @@
+//: [Previous](@previous)
+
+/**
+ # Interval List Intersections
+
+ Given two lists of **closed** intervals, each list of intervals is pairwise disjoint and in sorted order.
+
+ Return the intersection of these two interval lists.
+
+ *(Formally, a closed interval `[a, b]` (with `a <= b`) denotes the set of real numbers `x` with `a <= x <= b`. The intersection of two closed intervals is a set of real numbers that is either empty, or can be represented as a closed interval. For example, the intersection of [1, 3] and [2, 4] is [2, 3].)*
+
+  
+
+ **Example 1:**
+
+ **![img](https://assets.leetcode.com/uploads/2019/01/30/interval1.png)**
+
+ ```
+ Input: A = [[0,2],[5,10],[13,23],[24,25]], B = [[1,5],[8,12],[15,24],[25,26]]
+ Output: [[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]]
+ Reminder: The inputs and the desired output are lists of Interval objects, and not arrays or lists.
+ ```
+
+  
+
+ **Note:**
+
+ 1. `0 <= A.length < 1000`
+ 2. `0 <= B.length < 1000`
+ 3. `0 <= A[i].start, A[i].end, B[i].start, B[i].end < 10^9`
+
+ **NOTE:** input types have been changed on April 15, 2019. Please reset to default code definition to get new method signature.
+ */
+
+class Solution {
+    // 224ms, 58.37%
+    func intervalIntersection(_ A: [[Int]], _ B: [[Int]]) -> [[Int]] {
+        var i = 0, j = 0, ans = [[Int]]()
+        while i < A.count, j < B.count {
+            let v = (max(A[i][0], B[j][0]), min(A[i][1], B[j][1]))
+            if v.0 <= v.1 { ans.append([v.0, v.1]) }
+            A[i][1] < B[j][1] ? (i += 1) : (j += 1)
+        }
+        return ans
+    }
+}
+
+let s = Solution()
+s.intervalIntersection([[0,2],[5,10],[13,23],[24,25]], [[1,5],[8,12],[15,24],[25,26]])
+s.intervalIntersection([[5,10]], [[5,10]])
+
+
+//: [Next](@next)
---
+++ LeetCoding Challenge 5.playground/Pages/Sort Characters By Frequency.xcplaygroundpage/Contents.swift
@@ -0,0 +1,89 @@
+//: [Previous](@previous)
+
+/**
+ Sort Characters By Frequency
+ Given a string, sort it in decreasing order based on the frequency of characters.
+
+ Example 1:
+
+ Input:
+ "tree"
+
+ Output:
+ "eert"
+
+ Explanation:
+ 'e' appears twice while 'r' and 't' both appear once.
+ So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer.
+ Example 2:
+
+ Input:
+ "cccaaa"
+
+ Output:
+ "cccaaa"
+
+ Explanation:
+ Both 'c' and 'a' appear three times, so "aaaccc" is also a valid answer.
+ Note that "cacaca" is incorrect, as the same characters must be together.
+ Example 3:
+
+ Input:
+ "Aabb"
+
+ Output:
+ "bbAa"
+
+ Explanation:
+ "bbaA" is also a valid answer, but "Aabb" is incorrect.
+ Note that 'A' and 'a' are treated as two different characters.
+ */
+
+class Solution {
+    // 96ms, 90,91%, O(Nlog(N)), ???
+    func frequencySort0(_ s: String) -> String {
+        return s.reduce(into: [:]) {
+            $0[$1, default: 0] += 1
+        }.sorted {
+            $0.value > $1.value
+        }.reduce("") {
+            $0 + String(repeating: $1.key, count: $1.value)
+        }
+    }
+    
+    // 140ms, 33.33%, O(N)
+    func frequencySort(_ s: String) -> String {
+        var dict = [Character: Int](), maxValue = 0
+        for c in s {
+            dict[c, default: 0] += 1
+            maxValue = max(maxValue, dict[c]!)
+        }
+        var buckets = Array(repeating: [Character](), count: maxValue+1)
+        for (k, v) in dict {
+            buckets[v].append(k)
+        }
+        var i = buckets.count-1, ans = ""
+        while i > 0 {
+            buckets[i].forEach { ans += String(repeating: $0, count: i) }
+            i -= 1
+        }
+        return ans
+    }
+    
+    // 436ms
+    func frequencySort1(_ s: String) -> String {
+        var buckets = Array(repeating: [Character](), count: s.count+1)
+        for (k, v) in s.reduce(into: [:], { $0[$1, default: 0] += 1 }) {
+            buckets[v].append(k)
+        }
+        return buckets.enumerated().reversed().reduce("") { v, e in
+            v + e.element.reduce("") { $0 + String(repeating: $1, count: e.offset) }
+        }
+    }
+}
+
+let s = Solution()
+s.frequencySort("tree")
+
+
+//: [Next](@next)
